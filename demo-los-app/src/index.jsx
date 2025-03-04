c348bbe1cf603810cebc0e4f36e121c458ed8ce6
Ritesh Chaudhari <riteshschaudhari19@gmail.com>
2025-03-04 13:39:51 +0530

    feat(Demo): builing src

--- demo-los-app/src/index.jsx
+++ demo-los-app/src/index.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 // import * as Server from 'react-dom/server'
 import {createRoot} from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import Driver from './Driver'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
 // let Greet = () => <h1>Hello, world!</h1>
 // console.log(Server.renderToString(<Greet />))
 
 const ele = document.getElementById('root')
 const root = createRoot(ele)
-root.render(<BrowserRouter><App /></BrowserRouter>)
+root.render(<Driver journey={'prospect'} />)
